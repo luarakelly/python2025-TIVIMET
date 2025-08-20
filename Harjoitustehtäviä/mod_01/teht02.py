49afc927f1f29ad9682cb73d96439122ba8f5762
luarakelly <lua.kellly@gmail.com>
2025-08-20 21:43:34 +0300

    Conluded module 01 exercise 02

---
+++ Harjoitustehtäviä/mod_01/teht02.py
@@ -0,0 +1,7 @@
+# Luo itsellesi GitHub-käyttäjätili ja tee repositorio Python-harjoitustehtäviä varten. 
+# Määritä IDE käyttämään repositoriota harjoitustehtäväprojektin tietojen tallentamiseen. 
+# Varmista, että voit hakea, sitouttaa ja työntää tekemiäsi muutoksia (pull, commit, push).
+
+
+# Tehty git repositorio: https://github.com/luarakelly/python2025-TIVIMET
+#                        https://github.com/luarakelly/python2025-TIVIMET/commits/main/
