b3e6b96fc55414e799f0c249cdb3c2f26ab22499
luarakelly <lua.kellly@gmail.com>
2025-08-20 21:43:16 +0300

    Conluded module 01 exercise 01

---
+++ Harjoitustehtäviä/mod_01/teht01.py
@@ -0,0 +1,3 @@
+# Asenna IDE-kehitin. Kirjoita ohjelma, joka tervehtii sinua omalla nimelläsi. Jos nimesi olisi Viivi Virta, ohjelma tulostaisi: Hei, Viivi Virta!
+
+print("Hei, Luara Moreira da Silva!")
